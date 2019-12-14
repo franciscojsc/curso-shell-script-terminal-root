4a5e551d2ac2c72aee41c2297689fdada6658029
Francisco Chaves <francisco.jschaves@gmail.com>
2019-12-14 09:27:43 -0300

    Olá mundo Shell Script

---
+++ ola-mundo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Comentário em linha
+
+:<<COMENTARIO
+echo "Comentário em bloco"
+echo "teste1
+echo "teste2
+echo "teste3
+echo "teste4
+echo "teste5
+echo "teste6
+echo "teste7
+COMENTARIO
+
+echo "Olá, mundo!"
